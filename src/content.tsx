e1d940d51465a0de7993e69e0692cd16c39623d1
Mohammed Hammoud <hammoud.mohammed@outlook.com>
2022-12-21 00:45:41 +0100

    style: remove global styles

--- src/content.tsx
+++ src/content.tsx
@@ -1,7 +1,6 @@
 import { Content } from './components/Content';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { css, Global } from '@emotion/react';
 
 const root = document.createElement('div');
 root.id = 'crx-root';
@@ -9,13 +8,6 @@ document.body.append(root);
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <Global
-      styles={css`
-        #crx-root {
-          color: #000;
-        }
-      `}
-    />
     <Content />
   </React.StrictMode>
 );
